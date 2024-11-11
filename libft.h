7534c4b9ac58df33643d4d41966485ae1265e7d4
Sarah Behar <sbehar@c2r5p6.42nice.fr>
2024-11-11 17:45:14 +0100

    update all

---
+++ libft.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbehar <sbehar@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 17:27:38 by sbehar            #+#    #+#             */
+/*   Updated: 2024/11/11 17:40:00 by sbehar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdio.h>
+
+void	ft_bzero(void *s, size_t n);
+int		ft_isalnum(int i);
+int		ft_isalpha(int i);
+int		ft_isascii(int i);
+int		ft_isdigit(int i);
+int		ft_isprint(int i);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+#endif
